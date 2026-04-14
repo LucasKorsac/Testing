da828fdbde0c613eb99e1e4bb65b82f5ca9d219a
Лариса Минашкина <minashkinalarisa@outlook.com>
2026-04-14 13:41:01 +0300

    Паттерн одиночка - изменение

--- Testing/Pattern/Singleton.cs
+++ Testing/Pattern/Singleton.cs
@@ -9,20 +9,35 @@ using System.Transactions;
 
 namespace Testing.Pattern
 {
-    //Одиночка
-    //Единственный клиент Mongo
+    /// <summary>
+    /// Паттерн одиночка. Один клиент на всё приложение
+    /// </summary>
     public class Singleton
     {
-        private static MongoClient _client;
-        private static IMongoDatabase _database;
+        private static readonly Lazy<Singleton> _instance =
+            new(() => new Singleton());
 
-        static MongoContext()
+        private readonly MongoClient _client;
+        private readonly IMongoDatabase _database;
+
+        /// <summary>
+        /// Приватный конструктор, во избежание создания извне
+        /// </summary>
+        private Singleton()
         {
             _client = new MongoClient("mongodb://localhost:27017");
-            _database = _client.GetDatabase("test2");
+            _database = _client.GetDatabase("Test2");
         }
 
-        public static IMongoCollection<T> GetCollection<T>(string name)
+        /// <summary>
+        /// Точка доступа
+        /// </summary>
+        public static Singleton Instance => _instance.Value;
+
+        /// <summary>
+        /// Получение коллекции коллекцию
+        /// </summary>
+        public IMongoCollection<T> GetCollection<T>(string name)
         {
             return _database.GetCollection<T>(name);
         }
